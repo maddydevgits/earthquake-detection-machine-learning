44ffd796939ab9cfc2f2ec60bb725b89f170e51a
madblocksgit <madblocks2k19@gmail.com>
2022-06-25 22:38:54 +0530

    updated code

--- app.py
+++ app.py
@@ -15,35 +15,21 @@ for d, t in zip(data['Date'], data['Time']):
         timestamp.append(time.mktime(ts.timetuple()))
     except ValueError:
         # print('ValueError')
-        timestamp.append('ValueError')
+        timestamp.append(0)
+    except OverflowError:
+        timestamp.append(0)
+        
 timeStamp = pd.Series(timestamp)
 data['Timestamp'] = timeStamp.values
 final_data = data.drop(['Date', 'Time'], axis=1)
 final_data = final_data[final_data.Timestamp != 'ValueError']
 
-from mpl_toolkits.basemap import Basemap
-
-m = Basemap(projection='mill',llcrnrlat=-80,urcrnrlat=80, llcrnrlon=-180,urcrnrlon=180,lat_ts=20,resolution='c')
-
-longitudes = data["Longitude"].tolist()
-latitudes = data["Latitude"].tolist()
-#m = Basemap(width=12000000,height=9000000,projection='lcc',
-            #resolution=None,lat_1=80.,lat_2=55,lat_0=80,lon_0=-107.)
-x,y = m(longitudes,latitudes)
-
-fig = plt.figure(figsize=(12,10))
-plt.title("All affected areas")
-m.plot(x, y, "o", markersize = 2, color = 'blue')
-m.drawcoastlines()
-m.fillcontinents(color='coral',lake_color='aqua')
-m.drawmapboundary()
-m.drawcountries()
-plt.show()
 
 
 X = final_data[['Timestamp', 'Latitude', 'Longitude']]
 y = final_data[['Magnitude', 'Depth']]
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 print(X_train.shape, X_test.shape, y_train.shape, X_test.shape)
